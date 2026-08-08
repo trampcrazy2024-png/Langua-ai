2c39ed95dc86e586d1f1c687aaee01fda3f8cee9
DrSM9october <joo555717@gmail.com>
2026-08-08 22:11:54 +0330

    Update ConfigUpdateWorker.kt

--- app/src/main/java/com/example/vpn/worker/ConfigUpdateWorker.kt
+++ app/src/main/java/com/example/vpn/worker/ConfigUpdateWorker.kt
@@ -5,23 +5,18 @@ import android.util.Log
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.example.vpn.data.remote.RemoteConfigRepository
-import com.example.vpn.data.repository.ServerRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 
 @HiltWorker
 class ConfigUpdateWorker @AssistedInject constructor(
     @Assisted context: Context,
-    @Assisted workerParams: WorkerParameters,
-    private val remoteConfigRepository: RemoteConfigRepository,
-    private val serverRepository: ServerRepository
+    @Assisted workerParams: WorkerParameters
 ) : CoroutineWorker(context, workerParams) {
 
     override suspend fun doWork(): Result {
         return try {
             Log.i("ConfigUpdateWorker", "Starting background config update...")
-            val newConfigs = remoteConfigRepository.fetchConfigs()
             Result.success()
         } catch (e: Exception) {
             Log.e("ConfigUpdateWorker", "Error updating configs in background", e)
