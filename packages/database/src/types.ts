01221c0f514e8023a0193abdf07b9044a7045e38
DrSM9october <joo555717@gmail.com>
2026-08-14 11:47:56 +0330

    Create types.ts

---
+++ packages/database/src/types.ts
@@ -0,0 +1,22 @@
+export interface DatabaseRow {
+  [key: string]: unknown;
+}
+
+export interface DatabaseResult {
+  changes?: {
+    changes?: number;
+    lastId?: number;
+  };
+  values?: DatabaseRow[];
+}
+
+export interface Migration {
+  version: number;
+  name: string;
+  up(database: DatabaseExecutor): Promise<void>;
+}
+
+export interface DatabaseExecutor {
+  execute(sql: string, values?: unknown[]): Promise<DatabaseResult>;
+  query(sql: string, values?: unknown[]): Promise<DatabaseResult>;
+}
