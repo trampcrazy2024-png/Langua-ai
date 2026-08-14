36925fbf6a7377720a57454584249f2185d4c7b9
DrSM9october <joo555717@gmail.com>
2026-08-14 13:10:33 +0330

    Update index.ts

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,12 +1,14 @@
 export * from './types';
 export * from './DatabaseManager';
 
-export * from './migrations/Migration';
-export * from './migrations/MigrationRunner';
+export {
+  MigrationRunner
+} from './migrations/MigrationRunner';
+
 export * from './migrations/v001_initial';
 
 export * from './repositories/BaseRepository';
 export * from './repositories/ConversationRepository';
 export * from './repositories/MessageRepository';
+export * from './repositories/UserMessageRepository';
 export * from './repositories/VocabularyRepository';
-export * from './repositories/UserMemoryRepository';
